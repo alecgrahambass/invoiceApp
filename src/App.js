d9084435d3470b22f178f5d4f1fffe01ad70f43b
Alec Graham <alecgraham@Alecs-MBP-3.lan>
2022-09-02 12:53:48 -0700

    UI built built out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+     <main className="m-5 p-5 xl:max-w-4xl xl:mx-auto
+     bg-white rounded shadow">
+       {/* Header */}
+      <header className="flex flex-col items-center
+      justify-center mb-5 xl:flex-row xl:justify-between">
+        <div>
+          <h1 className="font-bold uppercase tracking-wide text-4xl mb-3">Invoicer</h1>
+        </div>
+
+        <div>
+          <ul className="flex items-center
+          justify-between flex-wrap">
+            <li><button className="btn btn-print">Print</button></li>
+            <li><button className="btn btn-download">Download</button></li>
+            <li><button className="btn btn-send">Send</button></li>
+          </ul>
+
+        </div>
       </header>
-    </div>
+      {/* End of header */}
+
+      {/* Your details */}
+      <section className="flex flex-col items-end justify-end">
+        <h2 className="text-xl uppercase">Alec Graham</h2>
+        <p>Your Address</p>
+      </section>
+      {/* End of your details */}
+
+      {/*Client details */}
+      <section className="mt-5">
+        <h2 className="text-xl uppercase">Client's Name</h2>
+        <p>Client's Address</p> 
+      </section>
+      {/* End Client Details */ }
+
+      {/* Dates */}
+      <article className=" my-5 flex items-end justify-end">
+        <ul>
+          <li><span className="font-bold">Invoice Number: </span> </li>
+          <li><span className="font-bold">Invoice Date: </span></li>
+          <li><span className="font-bold">Due Date: </span></li>
+        </ul>      
+      </article>
+      {/* End of Dates */}
+
+      {/* Table */}
+        <div className="my-5">This is the Table</div>
+      {/* End Table */}
+
+      {/* Notes */}
+        <section className="mb-5">
+        {/* text area */}
+        <p>Notes to client...</p>
+        </section>
+      {/* End of Notes */}
+     </main>
+    </>
   );
 }
 
 export default App;
+ 
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return ( 
+    <>
+      <footer>
+        <ul className="flex flex-wrap items-center
+        justify-center">
+          <li><span className="font-bold">Your Name:</span> Alec Graham</li>
+          <li><span className="font-bold">Your Email:</span> alecgrahambass@gmail.com </li>
+          <li><span className="font-bold">Phone Number:</span> 0712 345 678</li>
+          <li><span className="font-bold">Bank:</span> Wells Fargo</li>
+          <li><span className="font-bold">Account Holder:</span> Alec Graham</li>
+          <li><span className="font-bold">Bank Account Number:</span> 123 456 789</li>
+          <li><span className="font-bold">Zelle:</span> zelle thing</li>
+          <li><span className="font-bold">Venmo:</span> @alecgraham</li> 
+
+        </ul> 
+      </footer>
+    </>
+  )
+}
